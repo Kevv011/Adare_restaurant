5708f61c6d353c44905e0410ea9e07f1cfc66c70
Sergio Palacios <sergioarepalacios9264@gmail.com>
2024-10-16 22:29:08 -0600

    Últimos cambios a implementar en el proyecto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import "../assets/css/Footer.css"
+import adareLogo from "../assets/images/adareLogo.png"
 
 export default function FooterN() {
     return (
         <>
             <footer className="footer-lg container-fluid text-center">
-                <p className="display-6">Adaré restaurant</p>
-                <small>&copy; Todos los derechos reservados</small>
+                <section className="container">
+                    <article className="col-12">
+                        <img src={adareLogo} alt="adareLogo" />
+                        <h5 className="h5">(+503) 2205-0000</h5>
+                        <p>1a calle Oriente, Centro Histórico de San Salvador, El Salvador</p>
+                        <small>&copy; Todos los derechos reservados</small>
+                    </article>
+                </section>
             </footer>
         </>
     )
